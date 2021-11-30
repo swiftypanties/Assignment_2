debadddc8ca983ad97f146b53f2d3a8992d36483
root <root@Aviv-Legion.localdomain>
2021-11-30 19:24:00 +0200

    Minor modifications after self test

--- main.c
+++ main.c
@@ -5,11 +5,19 @@ int main(){
 
     int mat[10][10];
     char func = 'S';    // this is where we will store the value to preform functions. 'S' for start to initialize the loop.
-
+    printf("\nthis is the test matrix\n");
+    int i,j;
+    for(i=0; i<10; i++){
+        for(j=0; j<10; j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
     
     //main program is running in a while loop
     while(func != 'D') {
         // accepts A, B, C, D functions from user
+        printf("Enter an operation: A, B, C, D.");
         scanf("%c", &func);
 
         if(func == 'A'){
@@ -30,3 +38,15 @@ int main(){
     }
     return 0;
 }
+// int test[10][10] ={
+    //     {0, 3, 1, 0, 0, 2, 0, 0, 0, 0},
+    //     {3, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    //     {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 5, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 4, 1, 1},
+    //     {2, 0, 0, 0, 0, 0, 2, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 2, 0, 0, 0, 0},
+    //     {0, 0, 0, 5, 4, 0, 0, 0, 0, 2},
+    //     {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 1, 0, 0, 2, 0, 0}
+    // };
--- my_mat.c
+++ my_mat.c
@@ -1,17 +1,13 @@
 # include <stdio.h>
 # include "my_mat.h"
-#define INF 999999
+#define INF 99999
 
 int A(int arr[10][10]){
 
     int i, j;
-
     for (i = 0; i < 10; i++){
         for (j = 0; j < 10; j++){
             scanf("%d", &arr[i][j]);
-            if((arr[i][j] == 0) && (i != j)){   // modify 0 to be MAX_INTEGER anywhere but in the trace, to symbolize none existing edge properly. 
-                arr[i][j] = INF;
-            }
         }
     }
     return 0;
@@ -19,54 +15,76 @@ int A(int arr[10][10]){
 
 void B(int graph[][10], int start, int end){
     int copy[10][10], i, j, k;
-
-    for (i = 0; i < 10; i++)                    // create a copy to work inside the function(releasing memory when scope ends)
-        for (j = 0; j < 10; j++)
-            copy[i][j] = graph[i][j];
-
-    for(k=0; k<10; k++){                        // preforming floydwarshall on the copy
+    // create a copy to work inside the function(releasing memory when scope ends)
+    for (i = 0; i < 10; i++){       
+        // modify 0 to be MAX_INTEGER anywhere but in the trace, to symbolize none existing edge properly.             
+        for (j = 0; j < 10; j++){
+            if(i==j){
+                copy[i][j]=0;  
+            }
+            else if(graph[i][j]==0 ){
+                copy[i][j]=INF; 
+            }
+            else {
+                copy[i][j] = graph[i][j];
+            }
+        }
+    }                                           
+    // preforming floydwarshall on the copy
+    for(k=0; k<10; k++){                        
         for(i=0; i<10; i++){
             for(j=0; j<10; j++){
                 if(copy[i][k] + copy[k][j] < copy[i][j]){
-                    copy[i][j] = (copy[i][k] + copy[k][j]);
-                }
+                    copy[i][j] = copy[i][k] + copy[k][j];
+                } 
             }
         }
     }
-    if(copy[start][end] == INF){
-        printf("\nFalse\n");
+    if(copy[start][end] == INF || copy[start][end] == 0){
+        printf("\nFalse");
     }
     else{
-        printf("\nTrue\n");
+        printf("\nTrue");
     }
+    
 }
 
 void C(int graph[][10], int start, int end){
     int copy[10][10], i, j, k;
-
-    for (i = 0; i < 10; i++)                         // create a copy to work inside the function(releasing memory when ends)
-        for (j = 0; j < 10; j++)
-            copy[i][j] = graph[i][j];
-            
-    for(k=0; k<10; k++){                             // preforming floydwarshall on the copy
+    // create a copy to work inside the function(releasing memory when scope ends)
+    for (i = 0; i < 10; i++){                   
+        for (j = 0; j < 10; j++){      
+            // modify 0 to be MAX_INTEGER anywhere but in the trace, to symbolize none existing edge properly.          
+            if(i==j){
+                copy[i][j]=0;  
+            }
+            else if(graph[i][j]==0 ){
+                copy[i][j]=INF; 
+            }
+            else {
+                copy[i][j] = graph[i][j];
+            }
+        }
+    }    
+    // preforming floydwarshall on the copy    
+    for(k=0; k<10; k++){                             
         for(i=0; i<10; i++){
             for(j=0; j<10; j++){
-                if(copy[i][k] + copy[k][j] < copy[i][j]){
-                    copy[i][j] = (copy[i][k] + copy[k][j]);
+                 if(copy[i][k] + copy[k][j] < copy[i][j]){
+                    copy[i][j] = copy[i][k] + copy[k][j];
                 }
             }
         }
     }
-    if(copy[start][end] == INF){
-        printf("-1");
+
+    if(copy[start][end] == INF || copy[start][end] == 0){
+        printf("\n-1");
     }
     else{
-        printf("%d", copy[start][end]);
+        printf("\n%d", copy[start][end]);
     }
 }
-
-// printf("\n this is the matrix after floyd C\n");
-    // for(i=0; i<10; i++){
+ // for(i=0; i<10; i++){
     //     for(j=0; j<10; j++){
     //         printf("%d ", copy[i][j]);
     //     }
